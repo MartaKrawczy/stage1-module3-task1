65471c832653039e2edeb2121b5e4fa67db7cdd7
MartaKrawczy <martaakrawczykowska@gmail.com>
2023-01-12 15:30:22 +0100

    initial commit

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -5,22 +5,22 @@ import java.util.List;
 
 public class Generics {
 
-    //TODO: Refactor Method-1
+    //TODO: Refactor Method-1 Refactor boxingMethod to use only parameterized lists.
     public List boxingMethod(String name) {
-        List firstList = new ArrayList<>();
+        List <String> firstList = new ArrayList<>();
         firstList.add(name);
-        List secondList = new ArrayList<>();
+        List <Object> secondList = new ArrayList<>();
         secondList.add(firstList);
         return secondList;
     }
 
-    //TODO: Refactor Method-2
-    public Object genericMethod(Object data) {
+    //TODO: Refactor Method-2 Refactor genericMethod, it should take any specific object and return it.
+    public <T> T genericMethod(T data) {
         return data;
     }
 
-    //TODO: Refactor Method-3
-    public void cloneMethod(List consumer, List producer) {
+    //TODO: Refactor Method-3 Refactor cloneMethod, it should take two lists and be able to place objects from producer to consumer list.
+    public void cloneMethod(List <? extends Generics> consumer, List producer) {
         consumer.addAll(producer);
     }
 
